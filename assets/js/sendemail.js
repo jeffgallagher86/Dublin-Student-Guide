52c9ab4ab8a5b343e0a9d46dcbcebd15f1391b7f
jeffgallagher86 <jeffgallagher86@gmail.com>
2021-04-21 23:29:47 +0000

    Created email template and integrated emailjs on site

---
+++ assets/js/sendemail.js
@@ -0,0 +1,18 @@
+/* --------------------------------------------------------adapted from CI tutorial */
+
+function sendMail(contactForm) {
+    emailjs.send("default_service", "Dublin Student Guide", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "information_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response){
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
