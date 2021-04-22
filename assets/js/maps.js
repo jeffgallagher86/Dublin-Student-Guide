731e49402ce632767ca14acf31715548f35dc162
jeffgallagher86 <jeffgallagher86@gmail.com>
2021-04-22 18:54:06 +0000

    added extra map markers

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -4,13 +4,15 @@ function initMap() {
     var options = {
         zoom: 14,
         center: {
-            lat: 53.338101964692584,
-            lng: -6.259243506777292
+            lat: 53.34738507428953,
+            lng: -6.259331175190513
         }
     }
 
     // New map
     var map = new google.maps.Map(document.getElementById('map'), options);
+
+    var nomarkers = [];
 
     var markers = [{
             coords: {
@@ -30,14 +32,100 @@ function initMap() {
             coords: {
                 lat: 53.345595807152264,
                 lng: -6.263220879604568
-            }
+            },
+            content: '<p>Temple Bar</p>'
         },
         {
             coords: {
                 lat: 53.3434142084807,
                 lng: -6.271053182407339
-            }
-        }
+            },
+            content: '<p>Christ Church Cathederal</p>'
+        },
+        {
+            coords: {
+                lat: 53.343838316674955,
+                lng: -6.254603788682655
+            },
+            content: '<p>Trinity College</p>'
+        },
+        {
+            coords: {
+                lat: 53.338689730579766,
+                lng: -6.2666201598468865
+            },
+            content: '<p>DIT Aungier Street</p>'
+        },
+        {
+            coords: {
+                lat: 53.34071093445668,
+                lng: -6.2654705445027945
+            },
+            content: '<p>Dublin Business School</p>'
+        },
+        {
+            coords: {
+                lat: 53.35005547059088,
+                lng: -6.2520012156663896
+            },
+            content: '<p>Busaras</p>'
+        },
+        {
+            coords: {
+                lat: 53.34431348351751,
+                lng: -6.240082875190646
+            },
+            content: '<p>Bord Gais Energy Theatre</p>'
+        },
+        {
+            coords: {
+                lat: 53.35037606102821,
+                lng: -6.26771313101038
+            },
+            content: '<p>Cineworld</p>'
+        },
+        {
+            coords: {
+                lat: 53.34855869351882,
+                lng:  -6.2571889310104485
+            },
+            content: '<p>Abbey Theatre</p>'
+        },
+        {
+            coords: {
+                lat: 53.34521464940263,
+                lng: -6.267426590687993
+            },
+            content: '<p>The Temple Bar</p>'
+        },
+        {
+            coords: {
+                lat: 53.34393236893571,
+                lng: -6.263658802174667
+            },
+            content: '<p>The Stags Head</p>'
+        },
+        {
+            coords: {
+                lat: 53.34855869351882,
+                lng: -6.2571889310104485
+            },
+            content: '<p>Abbey Theatre</p>'
+        },
+        {
+            coords: {
+                lat: 53.335645790094866,
+                lng: -6.265416644503038
+            },
+            content: '<p>Devitts Pub</p>'
+        },
+        {
+            coords: {
+                lat: 53.34323047072809,
+                lng: -6.2645499733387
+            },
+            content: '<p>Boojum</p>'
+        },
 
     ];
 
